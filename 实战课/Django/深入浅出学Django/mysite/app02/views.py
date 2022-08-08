6709db86bc5ea86bc70088c49ee0a5c6cabc772a
ccmldl <1738407610@qq.com>
2022-08-08 16:58:18 +0800

    202208081657

--- 实战课/Django/深入浅出学Django/mysite/app02/views.py
+++ 实战课/Django/深入浅出学Django/mysite/app02/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.core.handlers.wsgi import WSGIRequest
 # Create your views here.
 
 
-def index(request):
-    return render(request, "app01/timer.html")
+def index(request: WSGIRequest):
+    print(request.method)
+    print(request.path)  # /
+    print(request.get_full_path())  # /?a=1
+    print(request.META.get("REMOTE_ADDR"))  # IP
+    # 请求数据
+    print(request.GET)
+    print(request.POST)
+    return HttpResponse("OK")
