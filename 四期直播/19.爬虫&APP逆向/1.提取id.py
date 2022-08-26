5fe0626a2a507259aa7e60a58d2e69779cd6cb00
ccmldl <1738407610@qq.com>
2022-08-26 17:01:57 +0800

    202208261701

--- 四期直播/19.爬虫&APP逆向/1.提取id.py
+++ 四期直播/19.爬虫&APP逆向/1.提取id.py
@@ -1,5 +1,9 @@
 import re
+import math
 import json
+import random
+import time
+
 import requests
 
 url = "https://www.bilibili.com/video/BV1N94y1R7K5"
@@ -8,6 +12,7 @@ headers = {
 }
 
 resp = requests.get(url, headers=headers)
+print(resp.cookies.get_dict())
 data = re.search(r'__INITIAL_STATE__=(?P<page>.+);\(function', resp.text).group("page")
 data_dict = json.loads(data)
 aid = data_dict["aid"]
@@ -15,3 +20,15 @@ cid = data_dict["videoData"]["cid"]
 print(aid)
 print(cid)
 
+data = ""
+for i in range(8):
+    v1 = math.ceil(16 * random.uniform(0, 1))
+    v2 = hex(v1)[2:].upper()
+    data += v2
+result = data.rjust(8, "0")
+
+e = int(time.time() * 1000)
+t = hex(e)[2:].upper()
+
+b_lsid = f"{result}_{t}"
+print(b_lsid)
