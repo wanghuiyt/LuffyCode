ecae6187472fa9b2e6b3ee261c0ae7aab02d6cb7
ccmldl <1738407610@qq.com>
2022-08-30 07:03:30 +0800

    202208300703

---
+++ 四期直播/19.爬虫&APP逆向/6.证书和java基础/1.Java字节数组转字符串.py
@@ -0,0 +1,32 @@
+# 字节列表
+byte_list = [-26, -83, -90, -26, -78, -101, -23, -67, -112]
+
+"""
+java字节：有符号 -128~127
+python: 无符号 0~255 
+"""
+
+# 字节列表->python的字节数组
+bs = bytearray()
+for item in byte_list:
+    if item < 0:
+        item += 256  # Java中是有符号的，python中是无符号的
+    bs.append(item)
+
+# python的字节数组 -> 编码 -> 字符串
+str_data = bs.decode("utf-8")  # data = bytes(bs)
+print(str_data)
+
+# 字符串转字节数组
+data = "张三懵逼了"
+data_bytes = data.encode("utf-8")
+
+data_list = bytearray()
+for item in data_bytes:
+    data_list.append(item)
+
+print(data_list)
+res = data_list.decode("utf-8")
+print(res)
+
+
