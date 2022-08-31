c1b218299c0dcd7ecaa3ac3c04e54459d9427219
ccmldl <1738407610@qq.com>
2022-09-01 07:30:11 +0800

    202209010730

---
+++ 四期直播/19.爬虫&APP逆向/7.安卓开发/app.py
@@ -0,0 +1,31 @@
+import uuid
+from hashlib import md5
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+def myMd5(data):
+    obj = md5()
+    obj.update(data.encode("utf-8"))
+    return obj.hexdigest()
+
+
+@app.route("/auth", methods=["POST"])
+def auth():
+    print(request.form)
+    # 1.获取各个数据
+    user = request.form.get("user")
+    pwd = request.form.get("pwd")
+    sign = request.form.get("sign")
+    # 2.sign签名的校验
+    if sign != myMd5(f"password{pwd}username{user}"):
+        return "失败"
+    # 3.根据用户名和密码去数据库校验
+
+    return jsonify({"status": True, "token": str(uuid.uuid4())})
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
+
