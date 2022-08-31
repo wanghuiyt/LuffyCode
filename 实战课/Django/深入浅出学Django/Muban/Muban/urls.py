d69ae25828bf48bee21b8262bfb9c87c0c53949a
ccmldl <1738407610@qq.com>
2022-08-31 17:31:25 +0800

    202208311731

--- 实战课/Django/深入浅出学Django/Muban/Muban/urls.py
+++ 实战课/Django/深入浅出学Django/Muban/Muban/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app01.views import index
+from app01.views import index, order
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', index),
+    path('order/', order),
 ]
--- 实战课/Django/深入浅出学Django/Muban/app01/views.py
+++ 实战课/Django/深入浅出学Django/Muban/app01/views.py
@@ -1,6 +1,6 @@
 import datetime
-
 from django.shortcuts import render
+
 
 # Create your views here.
 class Stu(object):
@@ -11,13 +11,30 @@ class Stu(object):
 
 def index(request):
     name = "yuan"
-    books  = ["西游记", "三国演义", "金瓶梅", "红楼梦"]
-    info = {"name": "yuan", "age":18}
+    books = ["西游记", "三国演义", "金瓶梅", "红楼梦"]
+    info = {"name": "yuan", "age": 18}
     stu = Stu("xiaoming", 23)
     now = datetime.datetime.now()
     age = 10
     filesize = 203456984
     link = '<a href="http://www.baidu.com">click</a>'
+    user = "yuan"
+    score = 98
+    book_list = [
+        {"id": 11, "name": "Python基础入门", "price": 130.00},
+        {"id": 17, "name": "GO基础入门", "price": 230.00},
+        {"id": 23, "name": "PHP基础入门", "price": 330.00},
+        {"id": 44, "name": "Java基础入门", "price": 730.00},
+        {"id": 51, "name": "C++基础入门", "price": 300.00},
+        {"id": 56, "name": "C#基础入门", "price": 100.00},
+        {"id": 57, "name": "前端基础入门", "price": 380.00},
+    ]
     # return render(request, "index.html", {"name": name, "books": lst, "info": info})
     return render(request, "index.html", locals())
+
+
+def order(request):
+    return render(request, "order.html")
+
+
 
