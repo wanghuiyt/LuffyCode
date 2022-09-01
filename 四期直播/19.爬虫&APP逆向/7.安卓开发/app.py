17e034426946616aac789e9cc7a12c11fd18668a
ccmldl <1738407610@qq.com>
2022-09-02 06:43:35 +0800

    202209020643

--- 四期直播/19.爬虫&APP逆向/7.安卓开发/app.py
+++ 四期直播/19.爬虫&APP逆向/7.安卓开发/app.py
@@ -13,19 +13,20 @@ def myMd5(data):
 
 @app.route("/auth", methods=["POST"])
 def auth():
-    print(request.form)
+    print(request)
     # 1.获取各个数据
-    user = request.form.get("user")
-    pwd = request.form.get("pwd")
-    sign = request.form.get("sign")
+    # user = request.form.get("user")
+    # pwd = request.form.get("pwd")
+    # sign = request.form.get("sign")
     # 2.sign签名的校验
-    if sign != myMd5(f"password{pwd}username{user}"):
-        return "失败"
+    # if sign != myMd5(f"password{pwd}username{user}"):
+    #     return "失败"
     # 3.根据用户名和密码去数据库校验
 
     return jsonify({"status": True, "token": str(uuid.uuid4())})
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
-
+    # app.run(host="192.168.0.121", ssl_context="adhoc")
+    # f78a7f44
+    app.run(host="192.168.0.121", port=5000)
